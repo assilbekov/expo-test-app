bfb387b1237b50f2c152819d6de8527f6afd0def
assilbekov <askhat.assilbekov@gmail.com>
2024-03-11 22:01:07 -0300

    ImageViewer added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
+import ImageViewer from '@/ImageViewer';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 const PlaceholderImage = require('./assets/background-image.png');
 
@@ -7,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -23,10 +24,5 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
 });
